ff090441af91d6da2726ce60c585168ddb5e96ad
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2019-12-10 22:12:01 +0100

    feat(header): min height

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,16 @@ import styled, { ThemeContext } from 'styled-components';
 import Container from './container';
 
 const Root = styled.nav`
+    min-height: 150px;
 `;
 
 const Header = () => {
     const themeContext = React.useContext(ThemeContext);
     return (
         <Container background={themeContext.color.primary}>
-            header
+            <Root>
+                header
+            </Root>
         </Container>
     );
 };
