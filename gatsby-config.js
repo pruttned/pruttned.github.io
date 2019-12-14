be2a6e00fe75772405c66161d2d786bdac3b3149
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2019-12-14 23:09:33 +0100

    feat(md): add prism

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,39 @@ module.exports = {
         path: `${__dirname}/src/blog-pages`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [{
+          resolve: `gatsby-remark-prismjs`,
+          options: { // https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-remark-prismjs
+            // defaults to 'language-' (e.g. <pre class="language-js">).
+            // If your site loads Prism into the browser at runtime,
+            // (e.g. for use with libraries like react-live),
+            // you may use this to prevent Prism from re-processing syntax.
+            // This is an uncommon use-case though;
+            // If you're unsure, it's best to use the default value.
+            classPrefix: "language-",
+            // This is used to allow setting a language for inline code
+            // (i.e. single backticks) by creating a separator.
+            // This separator is a string and will do no white-space
+            // stripping.
+            // A suggested value for English speakers is the non-ascii
+            // character '›'.
+            inlineCodeMarker: null,
+            // This lets you set up language aliases.  For example,
+            // setting this to '{ sh: "bash" }' will let you use
+            // the language "sh" which will highlight using the
+            // bash highlighter.
+            aliases: {},
+            showLineNumbers: false,
+            // If setting this to true, the parser won't handle and highlight inline
+            // code used in markdown i.e. single backtick code like `this`.
+            noInlineHighlight: false,
+          }
+        }]
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -57,6 +89,7 @@ module.exports = {
         omitGoogleFont: true // https://github.com/KyleAMathews/typography.js/issues/211
       },
     },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
