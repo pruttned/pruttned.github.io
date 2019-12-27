dabb1d8223c3a96aa1c7f22e24cec1d44627ebaa
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2019-12-27 17:25:33 +0100

    fix(globalStyle): fix wrong vert pos of icons

--- src/components/global-style.js
+++ src/components/global-style.js
@@ -5,6 +5,9 @@ const GlobalStyle = createGlobalStyle`
         background: ${p => p.theme.color.bodyBackground};
         color: ${p => p.theme.color.text};
     }
+    img, svg{
+        vertical-align: middle;
+    }
 `;
 
 export default GlobalStyle;
