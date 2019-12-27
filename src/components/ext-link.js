3c49924b94116a352559308da2800e99e4b5f5ba
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2019-12-27 18:49:34 +0100

    fix(extLink): add className

--- src/components/ext-link.js
+++ src/components/ext-link.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
-const ExtLink = ({ href, children, follow }) => (
-    <a href={href} target="blank" rel={['noopener', !follow && 'nofollow'].filter(x => x).join(' ')}>{children}</a>
+const ExtLink = ({ href, children, follow, className }) => (
+    <a href={href} target="blank" className={className} rel={['noopener', !follow && 'nofollow'].filter(x => x).join(' ')}>{children}</a>
 );
 
 export default ExtLink;
