b9ab99b1a0503f813a41d84d3cd200f48054a07b
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2019-12-27 19:50:51 +0100

    feat(md): add rel and target to links

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ module.exports = {
     title: `nede.dev_`,
     description: `Dev blog of Peter Ruttkay-Nedecký`,
     author: `Peter Ruttkay-Nedecký`,
-    authorShortDescription: 'I`m a freelance full stack software engineer and technology lead.' , 
-    twitter: 'https://twitter.com/PNedecky',  
-    linkedIn: 'https://www.linkedin.com/in/peter-ruttkay-nedeck%C3%BD-69296bb3/',  
+    authorShortDescription: 'I`m a freelance full stack software engineer and technology lead.',
+    twitter: 'https://twitter.com/PNedecky',
+    linkedIn: 'https://www.linkedin.com/in/peter-ruttkay-nedeck%C3%BD-69296bb3/',
     github: 'https://github.com/pruttned',
   },
   plugins: [
@@ -38,7 +38,7 @@ module.exports = {
             // you may use this to prevent Prism from re-processing syntax.
             // This is an uncommon use-case though;
             // If you're unsure, it's best to use the default value.
-            classPrefix: "language-",
+            classPrefix: 'language-',
             // This is used to allow setting a language for inline code
             // (i.e. single backticks) by creating a separator.
             // This separator is a string and will do no white-space
@@ -55,6 +55,13 @@ module.exports = {
             // If setting this to true, the parser won't handle and highlight inline
             // code used in markdown i.e. single backtick code like `this`.
             noInlineHighlight: false,
+          }
+        },
+        {
+          resolve: 'gatsby-remark-external-links',
+          options: {
+            target: '_blank',
+            rel: 'nofollow noopener'
           }
         }]
       }
