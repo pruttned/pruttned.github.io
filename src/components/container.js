405babcbf1b5c05f203900ca8851d989a1701b69
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2019-12-27 18:19:37 +0100

    feat(container): inc width

--- src/components/container.js
+++ src/components/container.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Root = styled.div`
     display: grid;
     background: ${p => p.background || 'none'};
-    grid-template-columns: auto [content-start] minmax(0px, ${p => p.narrow ? `680px` : `1000px`}) [content-end] auto;
+    grid-template-columns: auto [content-start] minmax(0px, ${p => p.narrow ? `${680 + p.theme.grid.gutter * 2}px` : '1000px'}) [content-end] auto;
 `;
 
 const Content = styled.div`
