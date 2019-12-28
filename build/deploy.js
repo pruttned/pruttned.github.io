1879946836f16c13a7ff432d55c9f035518fc10a
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2019-12-28 15:49:04 +0100

    chore(deploy): set target branch to master

--- build/deploy.js
+++ build/deploy.js
@@ -1,7 +1,8 @@
 const ghpages = require('gh-pages');
 
 ghpages.publish('public', {
-    message: 'Deploy'
+    message: 'Deploy',
+    branch: 'master'
 }, err => {
     if (err) {
         console.error(err);
